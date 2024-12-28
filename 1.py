db2b3d87dcc6bcfce78965bad893111de86b875f
trevenue44 <trevenue44.main@gmail.com>
2024-12-28 12:32:07 +0000

    solved 66

---
+++ 1.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/two-sum/description/
+
+"""
+- We're going to rely on the relationship between addition and subtraction
+- Suppose we have two numbers a + b = target
+    - This means that a = target - b
+    - And b = target - a
+
+- For any given number, 
+    the number to be added to it to get a target is the target - that number
+    let's refer to this as the compliment.
+
+- For this question, we'll go through the nums array and keep track of each number and idx
+- because each number could be a possible compliment to a later number
+
+- for each number we'll check if we've seen its compliment before, if yes, then we've the soln
+"""
+
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mapp = {}
+
+        for i in range(len(nums)):
+            difference = target - nums[i]
+            if difference in mapp:
+                return [mapp[difference], i]
+
+            mapp[nums[i]] = i
---
+++ 66.py
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/plus-one/description/
+
+"""
+Here, we're going to adopt the concept of addition in math.
+
+ 459
++  1
+-----
+ 460
+-----
+
+We add from right to left. 
+For each sum, there's a place value which is the actual value for that place and then there's a carry-on which is moved on and added to the next place.
+In the example above, when we add the units place 9 + 1, we get 1 0. 
+The 0 is the resulting units place value.
+The 1 is then carried on to be added to the 5 at the tens place, resulting in the 6. 
+    - Note: same carry-on and place value concept is applied to the resulting 6 at the tens position
+
+We're going to write a loop from right to left on the array of digits.
+For each place, we'll update the value with the place value and keep track of the carry-on value to be used for the next place.
+"""
+
+from typing import List
+
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry_on = 1
+        for i in range(-1, -len(digits) - 1, -1):
+            summ = digits[i] + carry_on
+            carry_on = summ // 10
+            digits[i] = summ % 10
+
+        if carry_on:
+            digits.insert(0, carry_on)
+
+        return digits
